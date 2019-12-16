1c37159fbf311f01ad1f5ae6e5e35a04fd0058aa
Parsa Darbandsari <53213288+ParsaDarbandsari@users.noreply.github.com>
2019-12-16 21:10:08 +1300

    bug/ISSUE-8 done

--- commands/directory_commands/goto_parent_directory.py
+++ commands/directory_commands/goto_parent_directory.py
@@ -7,15 +7,4 @@ class GoToParentDirectoryCommand(Command):
 	
 	def exec(self):
 		parent_directory = os.path.pardir
-		# parent_directory = os.getcwd()
-		# done = False
-		# letter_index = -1
-		#
-		# while not done:
-		# 	letter = parent_directory[letter_index]
-		# 	parent_directory = parent_directory[:-1]
-		#
-		# 	if letter == "\\":
-		# 		done = True
-		#
 		os.chdir(parent_directory)
