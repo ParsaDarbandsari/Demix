f30b8d6ff4ebe4c0498818245a37f4d8fe2c395e
Parsa Darbandsari <53213288+ParsaDarbandsari@users.noreply.github.com>
2019-12-19 17:56:33 +1300

    bug/ISSUE-9 test modification done

--- tests/test_goto_directory.py
+++ tests/test_goto_directory.py
@@ -10,3 +10,9 @@ def test_goto_directory_non_accessible_directory():
 	directory_name = 'hack'
 	GoToDirectoryCommand(directory_name).exec()
 	assert not os.getcwd() == f"C:\\Users\\parsa\\projects\\Demix\\tests\\{directory_name}"
+
+def test_goto_directory_space_in_name():
+	os.chdir("C:\\Users\\parsa\\projects\\Demix\\tests")
+	directory_name = 'test..dir'
+	GoToDirectoryCommand(directory_name).exec()
+	assert os.getcwd() == f"C:\\Users\\parsa\\projects\\Demix\\tests\\{directory_name.replace('..', ' ')}"
